79f845cb9549eab5800d372ddf68cd163f956e3b
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2022-10-27 14:50:58 +0200

    improve arg parse

--- src/args.js
+++ src/args.js
@@ -6,28 +6,28 @@ export function parse() {
     let arg = args[i].split('=');
     switch (arg[0]) {
       case 'auth_method':
-        process.env.AUTH_METHOD = arg[1];
+        process.env.AUTH_METHOD = arg.slice(1,);
         break;
       case 'bucket_name':
-        process.env.BUCKET_NAME = arg[1];
+        process.env.BUCKET_NAME = arg.slice(1,);
         break;
       case 'ops':
-        process.env.OPS = arg[1];
+        process.env.OPS = arg.slice(1,);
         break;
       case 'download_path':
-        process.env.DOWNLOAD_PATH = arg[1];
+        process.env.DOWNLOAD_PATH = arg.slice(1,);
         break;
       case 'dir':
-        process.env.DIR = arg[1];
+        process.env.DIR = arg.slice(1,);
         break;
       case 'file_filter':
-        process.env.FILE_FILTER = arg[1];
+        process.env.FILE_FILTER = arg.slice(1,);
         break;
       case 'upload_path':
-        process.env.UPLOAD_PATH = arg[1];
+        process.env.UPLOAD_PATH = arg.slice(1,);
         break;
       case 'upload_files':
-        process.env.UPLOAD_FILES = arg[1];
+        process.env.UPLOAD_FILES = arg.slice(1,);
         break;
       default:
     }
